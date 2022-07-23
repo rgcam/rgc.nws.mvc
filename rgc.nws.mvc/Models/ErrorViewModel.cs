0bd1fbf9dec284e354a758f362957e2bc9b8eb58
Ryan Cameron <45379493+rgcam@users.noreply.github.com>
2022-07-23 16:39:56 -0600

    Adding rest of the solution for git

---
+++ rgc.nws.mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rgc.nws.mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
